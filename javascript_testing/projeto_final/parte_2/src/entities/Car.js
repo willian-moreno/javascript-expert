20a6ac4b8a3f839031f0bbc6d8f064693fb962c0
willian-moreno <willian_moreno@outlook.com>
2022-09-27 13:29:58 -0300

    Projeto Final TDD e BDD | Parte 2

---
+++ javascript_testing/projeto_final/parte_2/src/entities/Car.js
@@ -0,0 +1,13 @@
+const Base = require('./base/Base');
+
+class Car extends Base {
+	constructor({ id, name, releaseYear, available, gasAvailable }) {
+		super({ id, name });
+
+		this.releaseYear = releaseYear;
+		this.available = available;
+		this.gasAvailable = gasAvailable;
+	}
+}
+
+module.exports = Car;
